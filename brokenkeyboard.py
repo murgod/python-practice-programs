0b85adeeecc19c4bdd0f0b2b920118a6c5b274d7
Akshay Murgod <40592593+murgod@users.noreply.github.com>
2019-11-12 14:59:23 -0500

    code not complete

---
+++ brokenkeyboard.py
@@ -0,0 +1,11 @@
+text = "Hello this is code signal"
+letter = ['e', 'i', 'o', 'u', 'h']
+
+
+def brokenkeyboard(text, letter):
+    text2 = text.split(" ")
+    print(text2[0])
+
+
+
+brokenkeyboard(text, letter)
